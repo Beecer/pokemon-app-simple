5dba5517a36587a652c81aa34d16e17382b8de5f
Beecer <94152383+Beecer@users.noreply.github.com>
2021-11-21 17:50:51 -0800

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood='pizza';
-document.write(favoriteFood);
+let pokemonList=[
+  {name: 'Golduck', height: 5, types: ['damp', 'cloud-nine', 'swift-swim'] },
+  {name: 'Wartorle', height: 6, types: ['rain-dish', 'torrent']},
+  {name: 'Pidgeot', height: 3, types: ['keen-eye', 'tangled-feet', 'big-pecks']}
+];
