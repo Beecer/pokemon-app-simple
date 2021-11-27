5bf2c02aa2ae11fefb22e3b8ef034031eb9e44f9
Beecer <94152383+Beecer@users.noreply.github.com>
2021-11-26 16:48:46 -0800

    created for loop with condition to output list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
-let pokemonList=[
-  {name: 'Golduck', height: 5, types: ['damp', 'cloud-nine', 'swift-swim'] },
-  {name: 'Wartorle', height: 6, types: ['rain-dish', 'torrent']},
-  {name: 'Pidgeot', height: 3, types: ['keen-eye', 'tangled-feet', 'big-pecks']}
+const pokemonList=[
+  {
+    name: 'Golduck',
+    height: 5,
+    types: ['damp', 'cloud-nine', 'swift-swim']
+  },
+  {
+    name: 'Wartorle',
+    height: 6,
+    types: ['rain-dish', 'torrent']
+  },
+  {
+    name: 'Pidgeot',
+    height: 3,
+    types: ['keen-eye', 'tangled-feet', 'big-pecks']
+  }
 ];
+
+//loop lists Pokemon with height attribute
+for (let i=0; i < pokemonList.length; i++) {
+  if(pokemonList[i].height > 5) {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')- Wow, that\'s big!)', '<br>');
+}else {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')','<br>');
+  }
+}
