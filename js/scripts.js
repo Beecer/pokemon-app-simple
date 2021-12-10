9a73b27b60a4ebe73e4f05c36a142800711b53a3
Beecer <94152383+Beecer@users.noreply.github.com>
2021-12-09 16:29:12 -0800

    Added if/else function add(pokemon), addListItem, and button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//IIFE wrap
 let pokemonRepository = (function () {
   let pokemonList = [
   {
@@ -17,25 +18,60 @@ let pokemonRepository = (function () {
   }
 ];
 
-  function add(pokemon) {
-    return pokemonList.push(pokemon);
-  }
 
+
+   function add(pokemon) {
+     if (
+       typeof pokemon === 'object'&&
+     "name" in pokemon &&
+     "height" in pokemon &&
+     "types" in pokemon
+   ) {
+     pokemonList.push(pokemon)
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
   function getAll() {
     return pokemonList;
   }
+  function showDetails(pokemon) {
+    return pokemonList;
+  }
 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon_list');
+    let listPokemon = document.createElement('li');
+    //button shows details of pokemon in console
+    let button = document.createElement('button');
+    button.addEventListener('click', function (showDetails){
+      console.log(pokemon);
+    });
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+  }
+  
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
 
 pokemonRepository.add({ name: 'Blastoise', height: 4, types: ['rain-dish', 'torrent']});
+pokemonRepository.add({name: 6 , height: 4, types: ['rain-dish', 'torrent']});
 
 
-function myLoopFunction(pokemon) {
-  document.write(pokemon.name + ' is ' + pokemon.height + '.','<br>','Types: ' + pokemon.types + '.', '<br>','<br>');
-}
-pokemonRepository.getAll().forEach(myLoopFunction);
+//buttons of each pokemon
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+//log pokemon details in console
+/*
+pokemonRepository.showDetails().forEach(function (pokemon) {
+  console.log(pokemon);
+});*/
